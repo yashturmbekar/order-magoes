cf6cd4a67e892a6aa015ee5fe35819355400d827
Yash <yash.turmbekar@quasardigital.com>
2025-04-15 22:10:10 +0530

    changes in url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,4 @@
 export const config = {
-  API_BASE:
-    process.env.NODE_ENV === "production"
-      ? "https://order-mangoes-backend-production.up.railway.app"
-      : "http://localhost:3000",
+  API_BASE: "https://order-mangoes-backend-production.up.railway.app"
+      //: "http://localhost:3000",
 };
