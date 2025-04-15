91f1a311424efb37fa454573be79debcfd351f57
Yash <yash.turmbekar@quasardigital.com>
2025-04-15 22:32:11 +0530

    reverted

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,6 @@
 export const config = {
-  API_BASE: "https://order-mangoes-backend-production.up.railway.app"
-      //: "http://localhost:3000",
+  API_BASE:
+    process.env.NODE_ENV === "production"
+      ? "https://order-mangoes-backend-production.up.railway.app"
+      : "http://localhost:3000",
 };
