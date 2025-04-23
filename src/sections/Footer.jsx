bd66344ad7acb3ed8ec9e4c24c6d479c70f42a93
Yash <yash.turmbekar@quasardigital.com>
2025-04-23 16:30:37 +0530

    changed footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,21 +1,27 @@
 import React from "react";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="footer" style={{ backgroundColor: "#fff8dc" }}>
+    <footer
+      className="footer"
+      style={{ background: "linear-gradient(to right, #fff8dc, #ffe0b2)" }}
+    >
       <h4>Get in Touch</h4>
       <div className="footer-details">
         <p>
           <strong>Yash Turmbekar</strong>
         </p>
         <p>
-          📞 <a href="tel:+918237381312">+91 82373 81312</a>
+          <FaPhoneAlt /> <a href="tel:+918237381312">+91 82373 81312</a>
         </p>
         <p>
-          📧{" "}
+          <FaEnvelope />{" "}
           <a href="mailto:yashturmbkar7@gmail.com">yashturmbkar7@gmail.com</a>
         </p>
-        <p>📍 Pune, Maharashtra</p>
+        <p>
+          <FaMapMarkerAlt /> Pune, Maharashtra
+        </p>
       </div>
       <p className="footer-note">
         © 2025 Mangoes At Your Doorstep | All rights reserved
